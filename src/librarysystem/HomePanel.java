4b805ed9c6e779fe113ec22ca4efab7fbc5705f9
nomi-github <nomindalain@gmail.com>
2023-12-07 12:15:57 -0600

    menu part2

---
+++ src/librarysystem/HomePanel.java
@@ -0,0 +1,17 @@
+package librarysystem;
+
+import javax.swing.JPanel;
+
+public class HomePanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 * @param librarySystem 
+	 */
+	public HomePanel(LibrarySystem librarySystem) {
+
+	}
+
+}
